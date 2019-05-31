b8208b821524dfa706064dc6d52eded689e4066a
菊池玲花 <r-kikuchi@i-enter.co.jp>
2019-05-31 10:09:33 +0900

    Initial Commit

---
+++ originalApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  originalApp
+//
+//  Created by 菊池 玲花 on 2019/05/31.
+//  Copyright © 2019 reika.kikuchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
